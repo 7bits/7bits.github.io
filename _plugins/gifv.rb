f6ddc642731da40aff6a708d977cdd0d65303cbd
Ilya Siganov <ilya.blan4@gmail.com>
2018-05-02 14:07:42 +0600

    tune plugins to show content 100% width

--- _plugins/gifv.rb
+++ _plugins/gifv.rb
@@ -1,20 +1,22 @@
 class Gifv < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+  Syntax = /^\s*([^\s]+)(\s+(\d+%?)\s+(\d+%?)\s*)?/
 
   def initialize(tagName, markup, tokens)
     super
 
     if markup =~ Syntax then
       @id = $1
-      @width  = $3.nil? ? 560 : $3.to_i
-      @height = $4.nil? ? 420 : $4.to_i
+      # @width  = $3.nil? ? 560 : $3.to_i
+      # @height = $4.nil? ? 420 : $4.to_i
     else
       raise "No YouTube ID provided in the \"youtube\" tag"
     end
   end
 
   def render(context)
-    """<video poster='//i.imgur.com/#{@id}h.jpg' preload='auto' autoplay='autoplay' muted='muted' loop='loop' webkit-playsinline='' style='width: #{@width}px; height: #{@height}px;'>
+    # style = "width: #{@width}px; height: #{@height}px;"
+    style = ""
+    """<video poster='//i.imgur.com/#{@id}h.jpg' preload='auto' autoplay='autoplay' muted='muted' loop='loop' webkit-playsinline='' style='#{style}'>
       <source src='//i.imgur.com/#{@id}.mp4' type='video/mp4'>
     </video>"""
   end
--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,20 +1,24 @@
 class YouTube < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+  Syntax = /^\s*([^\s]+)(\s+(\d+%?)\s+(\d+%?)\s*)?/
 
   def initialize(tagName, markup, tokens)
     super
 
     if markup =~ Syntax then
       @id = $1
-      @width  = $3.nil? ? 560 : $3.to_i
-      @height = $4.nil? ? 420 : $4.to_i
+      # @width  = $3.nil? ? 800 : $3
+      # @height = $4.nil? ? 600 : $4
     else
       raise "No YouTube ID provided in the \"youtube\" tag"
     end
   end
 
   def render(context)
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"https://www.youtube.com/embed/#{@id}?color=white&theme=light&rel=0&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>"
+    return <<~HTML
+    <div class="youtube-container">
+      <iframe class="youtube-video" src="https://www.youtube.com/embed/#{@id}?color=white&theme=light&rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>  
+    </div>
+    HTML
   end
 
   Liquid::Template.register_tag "youtube", self
